6d6930500287ed98ed3dc70b58b50384c42453d1
Jerry(이재헌) <ti6419@gmail.com>
2023-05-07 21:48:05 +0900

    Initial commit

---
+++ boot/data-processing-server/src/main/java/com/violetbeach/DataProcessingServerApplication.java
@@ -0,0 +1,13 @@
+package com.violetbeach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataProcessingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataProcessingServerApplication.class, args);
+	}
+
+}
