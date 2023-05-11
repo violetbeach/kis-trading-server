3e0abc98bce3a095f091d9ab8ef738823baa6b0b
Jerry(이재헌) <ti6419@gmail.com>
2023-05-11 23:12:55 +0900

    fix: DataProcessingServerApplication DefaultConstructor 추가

--- boot/data-processing-server/src/main/java/com/violetbeach/kistradingserver/DataProcessingServerApplication.java
+++ boot/data-processing-server/src/main/java/com/violetbeach/kistradingserver/DataProcessingServerApplication.java
@@ -1,9 +1,12 @@
 package com.violetbeach.kistradingserver;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@NoArgsConstructor
 class DataProcessingServerApplication {
 
 	public static void main(String[] args) {
