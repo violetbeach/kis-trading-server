d1ae3939f1c5cfd85e77def0af3dbcbbea08d612
Jerry(이재헌) <ti6419@gmail.com>
2023-05-17 21:32:31 +0900

    fix: Feign @RequestParam 미동작 -> @QueryMap

--- domain/chart/src/main/java/com/violetbeach/kistradingserver/domain/adapter/out/web/KISChartClient.java
+++ domain/chart/src/main/java/com/violetbeach/kistradingserver/domain/adapter/out/web/KISChartClient.java
@@ -1,13 +1,14 @@
 package com.violetbeach.kistradingserver.domain.adapter.out.web;
 
+import feign.QueryMap;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 @FeignClient(name = "kis-auth-client", url = "${apis.kis.url}")
 interface KISChartClient {
 
     @PostMapping("/uapi/domestic-stock/v1/quotations/inquire-time-itemchartprice")
-    GetMinutesChartResponse getMinutesChart(@RequestParam GetMinutesChartCommand issueTokenRequest);
+    GetMinutesChartResponse getMinutesChart(@QueryMap GetMinutesChartCommand command);
 
 }
