dbbdab3d8526aac942b4d0ae70b96257f7eeff46
Jerry(이재헌) <ti6419@gmail.com>
2023-05-20 10:47:53 +0900

    fix: ChartClient 미동작 해결 (PostMapping -> GetMapping)

--- domain/chart/src/main/java/com/violetbeach/kistradingserver/domain/adapter/out/web/KISChartClient.java
+++ domain/chart/src/main/java/com/violetbeach/kistradingserver/domain/adapter/out/web/KISChartClient.java
@@ -2,13 +2,13 @@ package com.violetbeach.kistradingserver.domain.adapter.out.web;
 
 import feign.QueryMap;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @FeignClient(name = "kis-auth-client", url = "${apis.kis.url}")
 interface KISChartClient {
 
-    @PostMapping("/uapi/domestic-stock/v1/quotations/inquire-time-itemchartprice")
+    @GetMapping("/uapi/domestic-stock/v1/quotations/inquire-time-itemchartprice")
     GetMinutesChartResponse getMinutesChart(@QueryMap GetMinutesChartCommand command);
 
 }
