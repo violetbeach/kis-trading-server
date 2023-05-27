ef20ad1d0ca4afb091a6aba685c639e9d9b6c119
Jerry(이재헌) <ti6419@gmail.com>
2023-05-27 11:37:10 +0900

    fix: cron 실행 표현식에서 주말 제외

--- boot/data-processing-server/src/main/java/com/violetbeach/kistradingserver/boot/processing/adapter/in/scheduler/DataProcessingJobScheduleConfig.java
+++ boot/data-processing-server/src/main/java/com/violetbeach/kistradingserver/boot/processing/adapter/in/scheduler/DataProcessingJobScheduleConfig.java
@@ -6,10 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.scheduling.quartz.SpringBeanJobFactory;
 
+import static org.quartz.CronScheduleBuilder.cronSchedule;
 import static org.quartz.JobBuilder.newJob;
 
 @Configuration
 class DataProcessingJobScheduleConfig {
+
+    private static final String cronExp = "0 32/30 9-17 ? * MON-FRI";
 
     @Bean
     public JobDetail dataProcessingJobDetail() {
@@ -21,15 +24,12 @@ class DataProcessingJobScheduleConfig {
 
     @Bean
     public Trigger dataProcessingJobTrigger(JobDetail dataProcessingJobDetail) {
-        SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder
-                .simpleSchedule()
-                .withIntervalInMinutes(1)
-                .repeatForever();
+        CronScheduleBuilder cronScheduleBuilder = cronSchedule(cronExp);
 
         return TriggerBuilder.newTrigger()
                 .forJob(dataProcessingJobDetail)
                 .withIdentity("dataProcessingJobTrigger")
-                .withSchedule(scheduleBuilder)
+                .withSchedule(cronScheduleBuilder)
                 .build();
     }
 
