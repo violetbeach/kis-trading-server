f73e02406a0d268e0f98b12913509fb28dacf192
Jerry(이재헌) <ti6419@gmail.com>
2023-05-28 11:57:19 +0900

    test: CandleEntityProcessorTest 추가

---
+++ boot/data-processing-server/src/test/java/com/violetbeach/kistradingserver/boot/processing/adapter/in/batch/job/CandleEntityProcessorTest.java
@@ -0,0 +1,34 @@
+package com.violetbeach.kistradingserver.boot.processing.adapter.in.batch.job;
+
+import com.violetbeach.kistradingserver.domain.domain.Candle;
+import org.junit.jupiter.api.Test;
+
+import static fixture.domain.CandleFixture.삼성전자_분봉;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class CandleEntityProcessorTest {
+
+    CandleEntityProcessor processor = new CandleEntityProcessor("000021");
+
+    @Test
+    void process() {
+        // given
+        Candle expected = 삼성전자_분봉.getCandle();
+
+        // when
+        CandleVO candleVO = processor.process(expected);
+
+        assertAll(
+                () -> assertThat(candleVO.stockCode()).isEqualTo("000021"),
+                () -> assertThat(candleVO.price()).isEqualTo(expected.getPrice()),
+                () -> assertThat(candleVO.highPrice()).isEqualTo(expected.getHighPrice()),
+                () -> assertThat(candleVO.lowPrice()).isEqualTo(expected.getLowPrice()),
+                () -> assertThat(candleVO.volume()).isEqualTo(expected.getVolume()),
+                () -> assertThat(candleVO.baseTime()).isEqualTo(expected.getBaseTime())
+        );
+
+
+    }
+
+}
