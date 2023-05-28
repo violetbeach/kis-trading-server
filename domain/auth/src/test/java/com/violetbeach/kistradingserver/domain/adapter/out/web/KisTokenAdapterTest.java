f01a63548040326f39c2ff87c8ec50967aa9a9de
Jerry(이재헌) <ti6419@gmail.com>
2023-05-28 11:49:54 +0900

    test; AuthAdapter,Service 테스트수정사항 반영

--- domain/auth/src/test/java/com/violetbeach/kistradingserver/domain/adapter/out/web/KisTokenAdapterTest.java
+++ domain/auth/src/test/java/com/violetbeach/kistradingserver/domain/adapter/out/web/KisTokenAdapterTest.java
@@ -1,37 +1,59 @@
 package com.violetbeach.kistradingserver.domain.adapter.out.web;
 
+import com.violetbeach.kistradingserver.domain.domain.Token;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class KisTokenAdapterTest {
-    private KisTokenAdapter adapter;
+    KisTokenAdapter adapter;
     @Mock
     private KISTokenClient kisTokenClient;
+    AuthKeys authKeys;
 
     @BeforeEach
     void setUp() {
-        AuthKeys authKeys = new AuthKeys("grantType", "appKey", "secretKey");
+        authKeys = new AuthKeys("grantType", "appKey", "secretKey");
         adapter = new KisTokenAdapter(kisTokenClient, authKeys);
     }
 
     @Test
-    void testIssueToken() {
+    @DisplayName("KisTokenClient의 issueToken()을 호출한다.")
+    void ItCallKisTokenClient() {
         // given
-        doNothing().when(kisTokenClient).issueToken(any(IssueTokenRequest.class));
+        IssueTokenResponse response = 토큰_발급_응답();
+        given(kisTokenClient.issueToken(any(IssueTokenRequest.class)))
+                .willReturn(response);
 
         // when
-        adapter.issueToken();
+        Token token = adapter.issueToken();
 
         // then
         verify(kisTokenClient, times(1)).issueToken(
-                new IssueTokenRequest("grantType", "appKey", "secretKey")
+                new IssueTokenRequest(authKeys.grantType(), authKeys.appKey(), authKeys.secretKey()));
+
+        assertAll(
+                () -> assertThat(token.accessToken()).isEqualTo(response.accessToken()),
+                () -> assertThat(token.type()).isEqualTo(response.type()),
+                () -> assertThat(token.expirationTime()).isEqualTo(response.expireDate())
         );
+    }
+
+    private IssueTokenResponse 토큰_발급_응답() {
+        LocalDateTime current = LocalDateTime.now();
+        return new IssueTokenResponse("token", current.plusDays(1), "Bearer");
     }
 }
--- domain/auth/src/test/java/com/violetbeach/kistradingserver/domain/application/service/IssueTokenServiceTest.java
+++ domain/auth/src/test/java/com/violetbeach/kistradingserver/domain/application/service/IssueTokenServiceTest.java
@@ -1,6 +1,7 @@
 package com.violetbeach.kistradingserver.domain.application.service;
 
 import com.violetbeach.kistradingserver.domain.application.port.out.IssueTokenPort;
+import com.violetbeach.kistradingserver.domain.domain.Token;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -28,16 +31,19 @@ class IssueTokenServiceTest {
     class issueToken {
 
         @Test
-        @DisplayName("IssueTokenPort.issueToken()을 호출한다.")
-        void ItThrowsNothing() {
+        @DisplayName("IssueTokenPort.issueToken()을 호출하고 결과를 그대로 반환한다.")
+        void ItCallIssueTokenPort_ItReturnSameToken() {
             // given
-            doNothing().when(issueTokenPort).issueToken();
+            Token exptected = mock(Token.class);
+            given(issueTokenPort.issueToken()).willReturn(exptected);
 
             // when
-            service.issueToken();
+            Token token = service.issueToken();
 
             // then
             verify(issueTokenPort, times(1)).issueToken();
+
+            assertThat(token).isEqualTo(exptected);
         }
 
     }
